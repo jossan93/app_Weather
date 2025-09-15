5e492939ce30188dce678b09d7f19489cc53d448
Josefine Klasson <jossanwowar93@gmail.com>
2025-09-15 20:18:41 +0200

    works

--- weather-app/app/index.tsx
+++ weather-app/app/index.tsx
@@ -1,15 +1,54 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput, Button} from "react-native";
+import React, { useState } from "react";
+
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
 export default function Index() {
+  const [city, setcity] = useState("");
+  const [weather, setweather] = useState<any>(null);
+
+  const getWeather = async () => {
+    try {
+      console.log("API_KEY used:", API_KEY);
+
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
+      const data = await response.json();
+      setweather(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Väder app</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Stad"
+      value={city}
+      onChangeText={setcity}
+    />
+
+     <Button title="Hämta väder" onPress={getWeather} />
+
+           {weather && weather.main && (
+        <View style={styles.result}>
+          <Text style={styles.text}>Stad: {weather.name}</Text>
+          <Text style={styles.text}>Temperatur: {weather.main.temp}°C</Text>
+          <Text style={styles.text}>
+            Väder: {weather.weather[0].description}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center", padding: 20 },
+  title: { fontSize: 24, fontWeight: "bold", marginBottom: 20 },
+  input: { borderWidth: 1, padding: 10, width: "80%", marginBottom: 10, borderRadius: 5 },
+  result: { marginTop: 20, alignItems: "center" },
+  text: { fontSize: 18 }
+});
